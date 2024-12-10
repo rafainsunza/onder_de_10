a46a127c9d809469b86ca78488042f4a0ce0f20a
rafainsunza <rafainsunza@outlook.com>
2024-12-10 15:07:34 +0100

    Style and implement translation of score input

--- src/javascript/custom-components/score-display/dt-score-display.js
+++ src/javascript/custom-components/score-display/dt-score-display.js
@@ -2,6 +2,7 @@ import html from './dt-score-display.html';
 import style from './dt-score-display.component.sass';
 
 import { translate, getActiveLanguage } from '../../modules/languages.js';
+import { players } from '../../modules/game-stats.js';
 
 const template = document.createElement('template');
 
@@ -18,7 +19,47 @@ class DtScoreDisplay extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
 
+        this.scoreInput = this.shadowRoot.querySelector('.score-input');
+        this.playersWrapper = this.shadowRoot.querySelector('.players-wrapper');
+        this.inputTitle = this.shadowRoot.querySelector('.score-input-title');
+        this.scoreSubmitButton = this.shadowRoot.querySelector('.add-button');
+
+        this.appendPlayerData();
+        const currentLanguage = getActiveLanguage();
+        translate(currentLanguage.score_input, this.inputTitle);
+        translate(currentLanguage.score_submit, this.scoreSubmitButton);
+
+        document.addEventListener('language-changed', (e) => this.handleLanguageChange(e));
     }
+
+    handleLanguageChange(e) {
+        translate(e.detail.score_input, this.inputTitle);
+        translate(e.detail.score_submit, this.scoreSubmitButton);
+    }
+
+    appendPlayerData() {
+        for (const player in players) {
+            const name = players[player].name;
+            const score = players[player].score
+
+            const container = document.createElement('div');
+            const nameDiv = document.createElement('div');
+            const scoreDiv = document.createElement('div');
+
+            container.classList.add('player-container');
+            nameDiv.classList.add('name');
+            scoreDiv.classList.add('score');
+
+            nameDiv.textContent = name;
+            scoreDiv.textContent = score;
+
+            container.appendChild(nameDiv),
+                container.appendChild(scoreDiv)
+
+            this.playersWrapper.appendChild(container);
+        }
+    }
+
 
 }
 
