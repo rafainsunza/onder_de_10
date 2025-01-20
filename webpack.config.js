cc2d6f2782bbfd3d27e874a1762a8e3e8786405a
rafainsunza <rafainsunza@outlook.com>
2025-01-20 13:48:50 +0100

    Version 1.0.0 built

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production';
